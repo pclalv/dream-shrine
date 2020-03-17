c855568dd2a8cb949d5d8c39fa3a7eb1b16e6039
paul alvarez <palvarez@greenhouse.io>
2020-03-17 09:31:19 -0400

    Get some basic cljfx working

--- src/dream_shrine/core.clj
+++ src/dream_shrine/core.clj
@@ -1,5 +1,19 @@
 (ns dream-shrine.core
-  (:gen-class))
+  (:gen-class)
+  (:require [cljfx.api :as fx]))
+
+(fx/on-fx-thread
+  (fx/create-component
+    {:fx/type :stage
+     :showing true
+     :title "Cljfx example"
+     :width 300
+     :height 100
+     :scene {:fx/type :scene
+             :root {:fx/type :v-box
+                    :alignment :center
+                    :children [{:fx/type :label
+                                :text "Hello world"}]}}}))
 
 (defn -main
   "I don't do a whole lot ... yet."
