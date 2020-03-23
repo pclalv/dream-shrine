883d4782458239e10528b382eaa770baab3fc504
paul alvarez <palvarez@greenhouse.io>
2020-03-22 21:44:48 -0400

    Play with cljfx

--- src/dream_shrine/core.clj
+++ src/dream_shrine/core.clj
@@ -2,8 +2,11 @@
   (:gen-class)
   (:require [cljfx.api :as fx]))
 
-(fx/on-fx-thread
- (fx/create-component
+(def *state
+  (atom {:title "App title"}))
+
+(defn root [_state]
+  (println "in root")
   {:fx/type :stage
    :showing true
    :title "Cljfx example"
@@ -14,9 +17,31 @@
                   :content {:fx/type :group
                             :children [{:fx/type :image-view
                                         :image {:url (str "file:"
-                                                          "/Users/paulalvarez/code/dream-shrine/images/test.png")}}]}}}}))
+                                                          "/Users/paulalvarez/code/dream-shrine/images/test.png")}}]}}
+           :on-zoom (fn [zoom-event]
+                      (let [zoom-factor (.getZoomFactor zoom-event)]
+                        (cond (< 1 zoom-factor)
+                              #_=> (prn "zoom in")
+                              (> 1 zoom-factor)
+                              #_=> (prn "zoom out")
+                              (= 1 zoom-factor)
+                              #_=> (prn "zoom none"))))}})
+
+
+(defn map-event-handler [event]
+  (case (:event/type event)
+    ::zoom-in (swap! *state assoc-in [:by-id (:id event) :done] (:fx/event event))))
+
+;; TODO: follow map events example in order to implement zoom
+;; https://cljdoc.org/d/cljfx/cljfx/1.6.7/doc/readme#map-events
+(def renderer
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc assoc :fx/type root)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (fx/mount-renderer
+   *state
+   (fx/create-renderer
+    :middleware (fx/wrap-map-desc assoc :fx/type root)
+    :opts {:fx.opt/map-event-handler map-event-handler})))
